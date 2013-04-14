fc78bebfe0c8a6a34a392bda39942a41cb5ee866
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2013-04-14 14:59:26 +0900

    Add home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MyDictionary::Application.routes.draw do
+  get "browse/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ MyDictionary::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'browse#index'
 
   # See how all your routes lay out with "rake routes"
 
