fcd2fc89ad7e8a5252e4ce12ac6024b1ab7e74bb
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2013-04-14 14:30:49 +0900

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyDictionary::Application.config.secret_token = '4ec4a97f97ea51dd234cca1cce1f736cfd49e0fb9e2921dbf3c5304ca9bacb33a08a7a682e4eb251045c32063fa9a3356e18125c616a267f00e6c7435b3f3bbd'
