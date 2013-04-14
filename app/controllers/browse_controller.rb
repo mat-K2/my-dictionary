d47154e7570cd258618051dd014d3f02179df793
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2013-04-14 21:16:41 +0900

    Fix layout

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,6 +1,8 @@
 require "scrapinger"
 
 class BrowseController < ApplicationController
+  layout false, :only => %w(home)
+
   def index
     @iframe_url = params[:url] || "/browse/home"
     @search_result = Scrapinger.search(params[:word]) if params[:word]
