a2ef91b827dd984104488df6887612491ab76b82
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2013-04-14 21:56:48 +0900

    Add ajax(search)

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -6,5 +6,9 @@ class BrowseController < ApplicationController
   def index
     @iframe_url = params[:url] || "/browse/home"
     @search_result = Scrapinger.search(params[:word]) if params[:word]
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
