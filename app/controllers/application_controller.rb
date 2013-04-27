f2741c1a5b48f6fef99dcadc86ba5f797f805a66
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2013-04-27 17:14:44 +0900

    デモ対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
+
+  def current_user
+    current_user = User.find_by_email("yamada@test.com")
+  end
 end
