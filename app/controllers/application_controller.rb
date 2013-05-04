4f3fa5f99c79547b03202ee07bb81b327bbf1afa
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2013-05-04 11:32:14 +0900

    Add feature(login)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  # before_filter :authenticate_user!
-
-  def current_user
-    current_user = User.find_by_email("yamada@test.com")
-  end
+  before_filter :authenticate_user!
 end
